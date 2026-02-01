4e3023043e2a1b91e05ff237d99460e148afdfb7
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-02-01 21:00:49 +0600

    completed task 4 of function

--- function-task.js
+++ function-task.js
@@ -30,14 +30,35 @@
 // Extra else { return "zero"; } দরকার নেই → সব number হয় odd বা even।
 
 
-function make_avg(arr) {
-    let add = 0;
-    for (let x=0; x <arr.length; x++){
-        add += arr[x];
-    }
-    return add / arr.length;
+// function make_avg(arr) {
+//     let add = 0;
+//     for (let x=0; x <arr.length; x++){
+//         add += arr[x];
+//     }
+//     return add / arr.length;
 
+// }
+
+// let arr = [10,20,30,40,50,60];
+// console.log(make_avg(arr));
+
+
+// Task-4
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+function count_zero(binary){
+
+    let count =0;
+    for (let x =0; x < binary.length; x++){
+       
+        if( binary[x] === "0"){   //binary[x] মানে হলো: string binary এর x নাম্বার index-এ থাকা character।
+            count ++;
+        }
+        
+    }
+    
+     return count;
 }
 
-let arr = [10,20,30,40,50,60];
-console.log(make_avg(arr));
+let binary = "10101010101010101";
+console.log(count_zero(binary));
